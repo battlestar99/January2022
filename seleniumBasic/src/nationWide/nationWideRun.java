1c0f12d1908949983c97bd4c4f38189e075ec568
“[Razin <anik@agile1tech.com>
2022-01-30 12:27:07 -0500

    Nation wide app working

--- seleniumBasic/src/nationWide/nationWideRun.java
+++ seleniumBasic/src/nationWide/nationWideRun.java
@@ -2,7 +2,9 @@ package nationWide;
 
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class nationWideRun {
@@ -11,6 +13,7 @@ public class nationWideRun {
 		
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Agile1Tech\\Desktop\\January_2022_WS\\seleniumBasic\\Lib\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
+		
 		driver.get("https://multiproduct.nationwide.com/multi-quote/getting-started");
 		
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
@@ -21,6 +24,14 @@ public class nationWideRun {
 		
 		Thread.sleep(2000);
 		
+		
+		WebElement firstName = driver.findElement(By.id("gs-first-name"));
+		
+		
+		boolean x = firstName.isDisplayed(); 
+		System.out.println(x);
+		
+		firstName.sendKeys("michael");
 
 	}
 
