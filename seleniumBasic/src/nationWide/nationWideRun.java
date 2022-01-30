986ca281c9392ea25f018dacdc2c796f85d233c8
“[Razin <anik@agile1tech.com>
2022-01-30 12:24:29 -0500

    nationwide

---
+++ seleniumBasic/src/nationWide/nationWideRun.java
@@ -0,0 +1,27 @@
+package nationWide;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class nationWideRun {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Agile1Tech\\Desktop\\January_2022_WS\\seleniumBasic\\Lib\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://multiproduct.nationwide.com/multi-quote/getting-started");
+		
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+		driver.manage().deleteAllCookies();
+		
+		driver.manage().window().maximize();
+		
+		Thread.sleep(2000);
+		
+
+	}
+
+}
