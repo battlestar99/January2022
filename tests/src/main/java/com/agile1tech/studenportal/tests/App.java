8d65177815dfc9781c7799ba919bba35eb4d42f6
“[Razin <anik@agile1tech.com>
2022-03-05 13:38:28 -0500

    code organization class

---
+++ tests/src/main/java/com/agile1tech/studenportal/tests/App.java
@@ -0,0 +1,13 @@
+package com.agile1tech.studenportal.tests;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
